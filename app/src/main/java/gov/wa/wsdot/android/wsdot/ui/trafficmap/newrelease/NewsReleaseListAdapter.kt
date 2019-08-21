67946cfde404da35d454d14e49aae6765a44c5d6
Logan Sims <simsl@wsdot.wa.gov>
2019-08-21 15:48:13 -0700

    WIP: news items

---
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/ui/trafficmap/newrelease/NewsReleaseListAdapter.kt
@@ -0,0 +1,67 @@
+package gov.wa.wsdot.android.wsdot.ui.trafficmap.newrelease
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import gov.wa.wsdot.android.wsdot.R
+import gov.wa.wsdot.android.wsdot.api.response.travelerinfo.NewsReleaseResponse
+import gov.wa.wsdot.android.wsdot.databinding.FerryScheduleItemBinding
+import gov.wa.wsdot.android.wsdot.databinding.NewsItemBinding
+import gov.wa.wsdot.android.wsdot.db.ferries.FerrySchedule
+import gov.wa.wsdot.android.wsdot.db.travelerinfo.NewsRelease
+import gov.wa.wsdot.android.wsdot.ui.common.recyclerview.DataBoundListAdapter
+import gov.wa.wsdot.android.wsdot.util.AppExecutors
+
+/**
+ * A RecyclerView adapter for [NewsRelease] class.
+ */
+class NewsReleaseListAdapter(
+    private val dataBindingComponent: DataBindingComponent,
+    appExecutors: AppExecutors,
+    private val newsClickCallback: ((NewsRelease) -> Unit)? // ClickCallback for item in the adapter
+) : DataBoundListAdapter<NewsRelease, NewsItemBinding>(
+    appExecutors = appExecutors,
+    diffCallback = object : DiffUtil.ItemCallback<NewsRelease>() {
+
+        override fun areItemsTheSame(oldItem: NewsRelease, newItem: NewsRelease): Boolean {
+            return oldItem.link == newItem.link
+        }
+
+        override fun areContentsTheSame(oldItem: NewsRelease, newItem:NewsRelease): Boolean {
+            return oldItem.description == newItem.description
+                    && oldItem.pubdate.time == newItem.pubdate.time
+                    && oldItem.title == newItem.title
+                    && oldItem.description == newItem.description
+                    && oldItem.link == newItem.link
+        }
+    }
+) {
+
+    override fun createBinding(parent: ViewGroup): NewsItemBinding {
+
+        val binding = DataBindingUtil.inflate<NewsItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.news_item,
+            parent,
+            false,
+            dataBindingComponent
+        )
+
+        binding.root.findViewById<View>(R.id.tap_view).setOnClickListener {
+            binding.newsItem?.let {
+                newsClickCallback?.invoke(it)
+            }
+        }
+
+        return binding
+    }
+
+    override fun bind(binding: NewsItemBinding, item: NewsRelease, position: Int) {
+        binding.newsItem = item
+    }
+
+}
---
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/ui/trafficmap/newrelease/NewsReleaseViewModel.kt
@@ -0,0 +1,29 @@
+package gov.wa.wsdot.android.wsdot.ui.trafficmap.newrelease
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import gov.wa.wsdot.android.wsdot.db.travelerinfo.NewsRelease
+import gov.wa.wsdot.android.wsdot.repository.NewsRepository
+import gov.wa.wsdot.android.wsdot.util.network.Resource
+import javax.inject.Inject
+
+class NewsReleaseViewModel @Inject constructor(newsRepository: NewsRepository) : ViewModel() {
+
+    private val repo = newsRepository
+
+    // mediator handles resubscribe on refresh
+    val news = MediatorLiveData<Resource<List<NewsRelease>>>()
+
+    private var newsLiveData : LiveData<Resource<List<NewsRelease>>> = newsRepository.loadNews(false)
+
+    init {
+        news.addSource(newsLiveData) { news.value = it }
+    }
+
+    fun refresh() {
+        news.removeSource(newsLiveData)
+        newsLiveData = repo.loadNews(true)
+        news.addSource(newsLiveData) { news.value = it }
+    }
+}
