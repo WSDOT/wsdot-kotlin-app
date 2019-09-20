22e129bfebd87ea1b0c354c1d9867426864db746
Logan Sims <simsl@wsdot.wa.gov>
2019-09-20 13:32:48 -0700

    Adds info links for SR 167 and 405 toll rates
    Adds refresh timer for toll rates

--- app/src/main/java/gov/wa/wsdot/android/wsdot/ui/tollrates/tollsigns/I405TollSignsFragment.kt
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/ui/tollrates/tollsigns/I405TollSignsFragment.kt
@@ -1,6 +1,14 @@
 package gov.wa.wsdot.android.wsdot.ui.tollrates.tollsigns
 
+import gov.wa.wsdot.android.wsdot.R
+
 class I405TollSignsFragment: TollSignsFragment() {
+    override fun getInfoLinkURL(): String {
+        return "https://www.wsdot.com/Tolling/405/rates.htm"
+    }
+    override fun getInfoLinkText(): String {
+        return getString(R.string.info_450)
+    }
     override fun getRoute(): Int {
         return 405
     }
