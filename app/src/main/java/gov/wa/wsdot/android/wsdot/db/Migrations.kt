db91e89cc2e48f2d7b5e971754eff3c8c5035c4d
Logan Sims <simsl@wsdot.wa.gov>
2020-04-22 12:01:01 -0700

    adds database v2 schema json and migration base

---
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/db/Migrations.kt
@@ -0,0 +1,10 @@
+package gov.wa.wsdot.android.wsdot.db
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE FerrySailingWithSpaces RENAME TO FerrySailingsWithStatus")
+    }
+}
