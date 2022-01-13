8f8442df9bc9b1227ae025236aec32cd55548a79
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-01-13 13:46:20 -0800

    Fix notification display issue
    Notifications were not displaying for API 31.
    
    Targeting S+ (version 31 and above) requires that one of FLAG_IMMUTABLE or FLAG_MUTABLE be specified when creating a PendingIntent.
    
    Resolves issue #129

--- app/src/main/java/gov/wa/wsdot/android/wsdot/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/service/MyFirebaseMessagingService.kt
@@ -4,7 +4,9 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.preference.PreferenceManager
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -71,6 +73,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun getNotificationIntent(data: MutableMap<String, String>): PendingIntent {
 
         val type = data["type"]
@@ -120,7 +123,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             }
         }
         return PendingIntent.getActivity(this, alertId?.toInt() ?: 0, intent,
-            PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
     }
 
     /**
