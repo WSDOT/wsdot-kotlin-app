bb896f713a62dbf3592992e1f33e7d8a6fcef2f7
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-04-05 16:39:47 -0700

    Show late night ferry schedules after midnight
    When current time is between 12:00 a.m to 3:00 a.m. ferry schedules displayed "schedule unavailable".
    
    Fixes issue by rolling back the day picker date to the previous day during this time slot so late-night users can check the ferry schedule.
    
    Issue #142

--- app/src/main/java/gov/wa/wsdot/android/wsdot/ui/common/viewmodel/SharedDateViewModel.kt
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/ui/common/viewmodel/SharedDateViewModel.kt
@@ -7,8 +7,21 @@ import javax.inject.Inject
 class SharedDateViewModel @Inject constructor() : SharedValueViewModel<Date>() {
 
     init {
-        // set to current date on start
-        setValue(Calendar.getInstance().time)
+
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        // display previous day's ferry schedule if time is between 12:00am-3:00am
+        if (hour < 3) {
+            calendar.add(Calendar.DAY_OF_YEAR, -1)
+            setValue(calendar.time)
+        }
+        else {
+
+            // set to current date on start
+            setValue(calendar.time)
+        }
     }
+
 
 }
