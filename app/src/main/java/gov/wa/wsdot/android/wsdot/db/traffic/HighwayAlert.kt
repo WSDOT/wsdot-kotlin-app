a9e720a8b234a88555e482660efce4c93e2d066e
Andrew Englehorn <engleha@wsdot.wa.gov>
2024-03-26 14:37:11 -0700

    Store nullable value for highway alert category
    Addresses issue with app crash when statewide alerts have null data.
    
    Issue #203

--- app/src/main/java/gov/wa/wsdot/android/wsdot/db/traffic/HighwayAlert.kt
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/db/traffic/HighwayAlert.kt
@@ -10,7 +10,7 @@ data class HighwayAlert(
     val roadName: String?,
     val priority: String,
     val travelCenterPriorityId: Int?,
-    val category: String,
+    val category: String?,
     val eventCategoryTypeDescription: String?,
     val startLatitude: Double,
     val startLongitude: Double,
